19690beebe5af1270be581dde2dacebae60e89ed
kjawo <konjaworski@gmail.com>
2019-11-11 11:42:22 +0100

    Add movies data from json

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="container">
+            <h1>Baza filmów</h1>
+            <MoviesTable :Movies="Movies"/>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    import axios from "axios";
+    // import Movies from './assets/movies.json'
+    import MoviesTable from "@/components/MoviesTable";
+
+    export default {
+        name: 'app',
+        components: {MoviesTable},
+        created() {
+            axios.get('/movies.json').then(response => {
+                this.Movies = response.data;
+                // eslint-disable-next-line no-console
+                console.log(this.Movies)
+            })
+        },
+        data () {
+            return {
+                Movies: []
+            }
+        },
+
+      // mounted(){
+      //   // eslint-disable-next-line no-console
+      //     console.log(Movies)
+      // }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    /*#app {*/
+    /*    font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+    /*    -webkit-font-smoothing: antialiased;*/
+    /*    -moz-osx-font-smoothing: grayscale;*/
+    /*    text-align: center;*/
+    /*    color: #2c3e50;*/
+    /*    margin-top: 60px;*/
+    /*}*/
 </style>
---
+++ src/components/MoviesTable.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h2>Movie table</h2>
+        <table class="responsive-table">
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>Production Year</th>
+                <th>Cast</th>
+                <th>Genres</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(Movie, index) in lessMovieTable" v-bind:key="index">
+                <td>"{{Movie.title}}"</td>
+                <td>{{Movie.year}}</td>
+                <td>{{Movie.cast}}</td>
+                <td>{{Movie.genres}}</td>
+            </tr>
+            </tbody>
+        </table>
+        <button @click="showMore">Show more</button>
+        <a @click="showMore" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "MoviesTable",
+        props: ['Movies'],
+        data() {
+            return {
+                showNumber: 10
+            }
+        },
+        methods: {
+            showMore() {
+                this.showNumber += 10
+            }
+
+        },
+        computed: {
+            lessMovieTable() {
+                return this.Movies.slice(0, this.showNumber)
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
